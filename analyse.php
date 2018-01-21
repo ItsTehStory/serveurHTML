0a7887d5b4ca7bda1bb5419890cf0a4ac8b9c0af
Philippe Ballandras <philippe.ballandras@hotmail.com>
2018-01-21 02:21:50 -0500

    Debut bd

--- analyse.php
+++ analyse.php
@@ -1,7 +1,19 @@
 <?php 
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
 	if($_POST["id"] == "test"){
-		echo "Voici la réponse";
+		$servername = "localhost";
+		$username = "username";
+		$password = "password";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password);
+
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		}
+		echo "Connected successfully";
+		//echo "Voici la réponse";
 		return;
 	}else{
 		echo $_POST["id"];
