39b41dfd9f4661e3f91b75bd76672352fd01b8d5
Philippe Ballandras <philippe.ballandras@hotmail.com>
2018-01-21 02:44:26 -0500

    Maxime

--- analyse.php
+++ analyse.php
@@ -12,8 +12,16 @@
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		}
-		echo "Connected successfully";
+		echo "Connected successfully, ";
 		//echo "Voici la réponse";
+		$sql = "CREATE DATABASE Hackaton";
+		if ($conn->query($sql) === TRUE) {
+			echo "Database created successfully";
+		} else {
+			echo "Error creating database: " . $conn->error;
+		}
+
+		$conn->close();
 		return;
 	}else{
 		echo $_POST["id"];
