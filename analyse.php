0d49d327a46716936eae7ecd74ecb808b8450c4e
Philippe Ballandras <philippe.ballandras@hotmail.com>
2018-01-21 03:13:57 -0500

    Create table

--- analyse.php
+++ analyse.php
@@ -4,22 +4,31 @@
 		$servername = "localhost";
 		$username = "root";
 		$password = "root";
-
+		$dbname = "Hackaton";
+		
 		// Create connection
-		$conn = new mysqli($servername, $username, $password);
-
+		$conn = new mysqli($servername, $username, $password, $dbname);
 		// Check connection
 		if ($conn->connect_error) {
 			die("Connection failed: " . $conn->connect_error);
 		}
-		echo "Connected successfully, ";
-		//echo "Voici la réponse";
-		$sql = "CREATE DATABASE Hackaton";
+
+		// sql to create table
+		$sql = "CREATE TABLE events (
+		nom VARCHAR(30) PRIMARY KEY,
+		typesport VARCHAR(30) NOT NULL, 	//nom, choix sport, nb personne, time and date, position (lat et lgt)
+		nombrepersonne int NOT NULL,
+		heure VARCHAR(30)NOT NULL,
+		date VARCHAR(30)NOT NULL,
+		latitude FLOAT NOT NULL,
+		longitude FLOAT NOT NULL
+		)";
+
 		if ($conn->query($sql) === TRUE) {
-			echo "Database created successfully";
+			echo "Table MyGuests created successfully";
 		} else {
-			echo "Error creating database: " . $conn->error;
-		}
+			echo "Error creating table: " . $conn->error;
+}
 
 		$conn->close();
 		return;
